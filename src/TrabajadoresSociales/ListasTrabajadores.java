c96edf5dff642326b2b54541f649fa98af473fa9
nloria26 <niloriaug@est.utn.cr>
2025-06-29 16:50:15 -0600

    Clase ListasTrabajadores
    Se crearon métodos de las listas de trabajadores

---
+++ src/TrabajadoresSociales/ListasTrabajadores.java
@@ -0,0 +1,23 @@
+
+package TrabajadoresSociales;
+
+
+public class ListasTrabajadores implements Listas<TrabajadorSocial> {
+
+    @Override
+    public boolean add(TrabajadorSocial t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(TrabajadorSocial t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TrabajadorSocial search(Object id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
