1af10a1d843e3defae10f46a6e62ff98fb214c1c
CodaChRod <Bychaverridaniel@gmail.com>
2025-07-03 12:26:04 -0600

    Prueba
    prueba de subida

---
+++ src/Victimas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+/**
+ *
+ * @author bycha
+ */
+public class Victimas {
+    public String Cedula;
+    public String Nombre;
+    public DatetimeFormatter FechaNacimiento;
+    public String Gender;
+    private String Email;
+    private int Phone;
+    private String Location;
+}
